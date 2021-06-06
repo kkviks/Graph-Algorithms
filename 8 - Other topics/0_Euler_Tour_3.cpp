4290bede27f24ba027cf991368ab98fc55525630
kkviks <kkviks@gmail.com>
2021-06-06 10:24:05 +0530

    Add Euler Tour 3

---
+++ 8 - Other topics/0_Euler_Tour_3.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Euler tour of a tree
+
+class Graph
+{
+    vector<vector<int>> l;
+
+public:
+    Graph(int n)
+    {
+        l.reserve(n);
+    }
+
+    void addEdge(int u, int v)
+    {
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+
+    void euler_tour_3(int node, int parent)
+    {
+        cout << node << " "; // Only when we enter the node
+        for (auto nbr : l[node])
+        {
+            if (nbr != parent)
+            {
+                euler_tour_3(nbr, node);
+            }
+        }
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    Graph g(n + 1);
+
+    int e = n - 1; //becasue tree
+
+    for (int i = 0; i < e; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        g.addEdge(u, v);
+    }
+
+    g.euler_tour_3(1, -1);
+}
